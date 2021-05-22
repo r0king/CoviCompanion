003cca72fe83390f19b503bee22c29fd524c5064
Royal Babu <>
2021-05-22 15:32:33 +0000

    monogdb is back maybe

--- commands/register.js
+++ commands/register.js
@@ -1,6 +1,64 @@
 // const Discord = require('discord.js');
 const regex = new RegExp;
 const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient;
+
+async function dbWrite(userData) {
+
+
+  const client = new MongoClient(process.env.ATLAS, {
+    useUnifiedTopology: true
+  });
+
+  try {
+
+    await client.connect();
+    await actOnData(client);
+    await listDatabases(client);
+
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+
+}
+
+async function actOnData(client) {
+
+  const dataBase = await client.db("Covid_UserTest");
+  // const collection = dataBase.collection("User")
+  const inserted =await  collection.insertOne(
+    userData
+  );
+  console.log(console.ObjectID);
+  let addressCollection = await collection.find({});
+  while (await addressCollection.hasNext()) {
+    let Address = await addressCollection.next();
+    console.log(Address);
+
+  }
+
+  // let updateUser = await collection.updateOne({
+  //   "name": "Username"
+
+  // }, {
+  //   "$set": {
+  //     "name": "Rkng"
+  //   },
+  //   // "$addToSet":{
+  //   //   "name" : "Username"
+  //   // },
+  //   // "$push":{
+  //   //   "Address" : [1,2,3,4,5,6]
+  //   // }
+  // })
+  // console.log(updateUser.modifiedCount);
+
+
+
+}
+
 module.exports = {
   name: '!register',
   description: 'registeration',
@@ -43,7 +101,7 @@ module.exports = {
       console.log(`collected ${collected.size} messages`)
       // console.log(collected );
       let counter = 0;
-     
+
       // console.log(collected.content);
       // console.log(collector);
 
@@ -57,21 +115,37 @@ module.exports = {
       collected.forEach((value) => {
         let detail = details[counter++].split(" ")
         // console.log(detail[detail.length - 1], value.content);
-       
-        userdetail.push(detail[detail.length - 1] + ' ' + value.content) ;
-       
-        msg.channel.send(detail[detail.length - 1]  + ' '+value.content);
+
+        userdetail.push(detail[detail.length - 1] + ' ' + value.content);
+
+        msg.channel.send(detail[detail.length - 1] + ' ' + value.content);
       })
+
+      msg.reply(`Thank you for registering!!`);
       // console.log(userdetail)
       data[msg.author.id] = userdetail;
       console.log(data)
-      var userData = fs.readFileSync('data.json');
-      var jsonObj= JSON.parse(userData);
-      jsonObj.push(data)
-      msg.reply(`Thank you for registering!!`);
+      dbWrite(data)
+      // var userData = fs.readFileSync('data.json');
+      // var jsonObj = JSON.parse(userData);
+      // jsonObj.push(data)
+      // var newData = JSON.stringify(myObject);
+      // fs.writeFile('data.json', newData, err => {
+
+      //   if (err) throw err;
+
+      //   console.log("[+] Added user ");
+      // });
     })
-    
+
   },
 
 
 }
+
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
--- commands/testDatabase.js
+++ commands/testDatabase.js
@@ -24,80 +24,80 @@
 
 // console.log("01");
 
-// const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 
 
-// async function main() {
+async function main() {
 
 
-//   const client = new MongoClient(process.env.ATLAS, {
-//     useUnifiedTopology: true
-//   });
+  const client = new MongoClient(process.env.ATLAS, {
+    useUnifiedTopology: true
+  });
 
-//   try {
+  try {
 
-//     await client.connect();
-//     await actOnData(client);
-//     await listDatabases(client);
+    await client.connect();
+    await actOnData(client);
+    await listDatabases(client);
 
-//   } catch (e) {
-//     console.error(e);
-//   } finally {
-//     await client.close();
-//   }
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
 
-// }
-// async function actOnData(client) {
+}
+async function actOnData(client) {
 
-//   const dataBase = await client.db("Covid_UserTest");
-//   const collection = dataBase.collection("User")
-//   // const inserted =await  collection.insertOne(
-//   //   {
-//   //     "name":"Username",
-//   //     "number" : 123456789,
-//   //     "Address":[
-//   //       1235234321,
-//   //       "House",
-//   //       "Village",
-//   //       "District"
+  const dataBase = await client.db("Covid_UserTest");
+  const collection = dataBase.collection("User")
+  // const inserted =await  collection.insertOne(
+  //   {
+  //     "name":"Username",
+  //     "number" : 123456789,
+  //     "Address":[
+  //       1235234321,
+  //       "House",
+  //       "Village",
+  //       "District"
 
-//   //     ]
-//   //   }
-//   // );
-//   console.log(console.ObjectID);
-//   let addressCollection = await collection.find({});
-//   while (await addressCollection.hasNext()) {
-//     let Address = await addressCollection.next();
-//     console.log(Address);
+  //     ]
+  //   }
+  // );
+  console.log(console.ObjectID);
+  let addressCollection = await collection.find({});
+  while (await addressCollection.hasNext()) {
+    let Address = await addressCollection.next();
+    console.log(Address);
 
-//   }
+  }
 
-//   let updateUser = await collection.updateOne({
-//     "name": "Username"
+  let updateUser = await collection.updateOne({
+    "name": "Username"
 
-//   }, {
-//     "$set": {
-//       "name": "Rkng"
-//     },
-//     // "$addToSet":{
-//     //   "name" : "Username"
-//     // },
-//     // "$push":{
-//     //   "Address" : [1,2,3,4,5,6]
-//     // }
-//   })
-//   console.log(updateUser.modifiedCount);
+  }, {
+    "$set": {
+      "name": "Rkng"
+    },
+    // "$addToSet":{
+    //   "name" : "Username"
+    // },
+    // "$push":{
+    //   "Address" : [1,2,3,4,5,6]
+    // }
+  })
+  console.log(updateUser.modifiedCount);
 
 
 
-// }
+}
 
-// async function listDatabases(client) {
-//   databasesList = await client.db().admin().listDatabases();
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
 
-//   console.log("Databases:");
-//   databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-// };
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
 
 // module.exports = {
 //   main : main().catch(console.error)
