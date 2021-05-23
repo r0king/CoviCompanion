57885b6b66bf184086b0bbe52e942f8ed15833e7
Royal <>
2021-05-23 10:53:44 +0000

    database configured

--- commands/checkslot.js
+++ commands/checkslot.js
@@ -1,14 +1,5 @@
 const axios = require('axios')
 
-// const {
-//     getStates,
-//     getListofDistricts,
-//     findByPin,
-//     findByDistrict,
-//     calendarByPin,
-//     calendarByDistrict
-// } = require('./cowinapi')
-
 module.exports = {
   name: 'checkslot',
   description: 'checkslot!',
@@ -51,52 +42,44 @@ module.exports = {
   },
 };
 
-// const axios = require('axios')
-// const {
-//     getStates,
-//     getListofDistricts,
-//     findByPin,
-//     findByDistrict,
-//     calendarByPin,
-//     calendarByDistrict
-// } = require('./cowinapi')
-// module.exports = {
-//   name: 'checkslot',
-//   description: 'checkslot!',
-//   execute(msg, args) {
-//     msg.reply("pong");
-//     axios({
-//       method: 'get',
-//       url: 'https://cdn-api.co-vin.in/api/v2/admin/location/states',
-//       headers: {
-//           'accept': 'application/json',
-//           'Accept-Language': 'en_US',
-//           'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
-//       }
-//   })
-//       .then(function (response) {
-//           if (response.data.states !== undefined) {
-//             let allstates ='States:\n\t'
-//             response.data.states.forEach((k, v) => 
-//             { 
-//               allstates += `\t${v}: ${k.state_name} \n`;
-//               }
-//               );
-//               msg.channel.send(allstates);
-//             return {
-//                   status: true,
-//                   result: response.data.states
-//               }
-//           } else {
-//               throw 'e'
-//           }
-//       })
-//       .catch(function (error) {
-//           return {
-//               status: false,
-//               message: "Sorry that's an error"
-//           }
-//       })
 
-//   },
-// };
+async function dbWrite(id) {
+
+
+  const client = new MongoClient(process.env.ATLAS, {
+    useUnifiedTopology: true
+  });
+
+  try {
+
+    await client.connect();
+    console.log("[+] dataBase connected")
+    await actOnData(client,id);
+
+  } catch (e) {
+    console.error(e);
+    console.log("[-] dataBase error")
+  } finally {
+    console.log("[+] dataBase closing")
+    await client.close();
+  }
+
+}
+
+async function actOnData(client,id) {
+
+  const dataBase = await client.db("Covid_UserTest");
+  const collection = dataBase.collection("User01");
+  const options = { upsert: true };
+
+  console.log("[+] Looking for user")
+  console.log(console.ObjectID);
+
+  let addressCollection = await collection.find({
+    'id':id
+  })
+  if (addressCollection.hasNext())
+    return addressCollection.next();
+  else 
+    return 1;
+}
--- commands/register.js
+++ commands/register.js
@@ -3,62 +3,7 @@ const regex = new RegExp;
 const fs = require('fs');
 const MongoClient = require('mongodb').MongoClient;
 
-async function dbWrite(userData) {
-
-
-  const client = new MongoClient(process.env.ATLAS, {
-    useUnifiedTopology: true
-  });
-
-  try {
-
-    await client.connect();
-    await actOnData(client);
-    await listDatabases(client);
-
-  } catch (e) {
-    console.error(e);
-  } finally {
-    await client.close();
-  }
-
-}
-
-async function actOnData(client) {
-
-  const dataBase = await client.db("Covid_UserTest");
-  // const collection = dataBase.collection("User")
-  const inserted =await  collection.insertOne(
-    userData
-  );
-  console.log(console.ObjectID);
-  let addressCollection = await collection.find({});
-  while (await addressCollection.hasNext()) {
-    let Address = await addressCollection.next();
-    console.log(Address);
-
-  }
-
-  // let updateUser = await collection.updateOne({
-  //   "name": "Username"
-
-  // }, {
-  //   "$set": {
-  //     "name": "Rkng"
-  //   },
-  //   // "$addToSet":{
-  //   //   "name" : "Username"
-  //   // },
-  //   // "$push":{
-  //   //   "Address" : [1,2,3,4,5,6]
-  //   // }
-  // })
-  // console.log(updateUser.modifiedCount);
-
-
-
-}
-
+let data = {}
 module.exports = {
   name: '!register',
   description: 'registeration',
@@ -80,13 +25,13 @@ module.exports = {
     let counter = 0;
     let collector = msg.channel.createMessageCollector(filter, {
       max: details.length,
-      time: 1000 * 20
+      time: 1000 * 200
     });
 
     msg.channel.send(details[counter])
     collector.on('collect', m => {
       if (counter < details.length) {
-        m.channel.send(details[++counter])
+        msg.channel.send(details[++counter])
         // var x = msg.content;
         // if(counter === 1){
         //   if (isNaN(x))
@@ -110,22 +55,26 @@ module.exports = {
       //   let collect = collected.next();
       //   console.log(collect);
       // }
-      data = {}
       userdetail = [];
+      userdetail_send = '';
       collected.forEach((value) => {
         let detail = details[counter++].split(" ")
         // console.log(detail[detail.length - 1], value.content);
 
         userdetail.push(detail[detail.length - 1] + ' ' + value.content);
-
-        msg.channel.send(detail[detail.length - 1] + ' ' + value.content);
+        userdetail_send += `\t${detail[detail.length - 1]} ${value.content}  \n`;
       })
 
+        msg.channel.send(userdetail_send);
+
       msg.reply(`Thank you for registering!!`);
+      data['id'] = msg.author.id;
       // console.log(userdetail)
-      data[msg.author.id] = userdetail;
+      data["details"] = userdetail;
       console.log(data)
-      dbWrite(data)
+      
+      dbWrite(msg.author.id)
+
       // var userData = fs.readFileSync('data.json');
       // var jsonObj = JSON.parse(userData);
       // jsonObj.push(data)
@@ -143,9 +92,82 @@ module.exports = {
 
 }
 
-async function listDatabases(client) {
-  databasesList = await client.db().admin().listDatabases();
+async function dbWrite(id) {
 
-  console.log("Databases:");
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
+
+  const client = new MongoClient(process.env.ATLAS, {
+    useUnifiedTopology: true
+  });
+
+  try {
+
+    await client.connect();
+    console.log("[+] dataBase connected")
+    await actOnData(client,id);
+    // await listDatabases(client);
+
+  } catch (e) {
+    console.error(e);
+    console.log("[-] dataBase error")
+  } finally {
+    console.log("[+] dataBase closing")
+    await client.close();
+  }
+
+}
+
+async function actOnData(client,id) {
+
+  const dataBase = await client.db("Covid_UserTest");
+  const collection = dataBase.collection("User01");
+  const options = { upsert: true };
+
+  console.log("[+] Creating user")
+  console.log(console.ObjectID);
+
+  collection.updateOne(
+    {
+    'id':id
+  }, 
+  { $set: {
+    id:data.id,
+    details:data.details
+  }},
+  options);
+  let addressCollection = await collection.find({
+    'id':id
+  })
+  // .then(items => {
+  //   console.log(` ${items.length}`)
+  //   items.forEach(console.log)
+   
+  // })
+  // .catch( (err) =>{
+  //   const inserted = collection.insertOne(data);
+  // }
+  // )
+  while (await addressCollection.hasNext()) {
+    let Address = await addressCollection.next();
+    console.log(Address);
+
+  }
+
+  // let updateUser = await collection.updateOne({
+  //   "name": "Username"
+  // }, {
+  //   "$set": {
+  //     "name": "Rkng"
+  //   },
+  //   // "$addToSet":{
+  //   //   "name" : "Username"
+  //   // },
+  //   // "$push":{
+  //   //   "Address" : [1,2,3,4,5,6]
+  //   // }
+  // })
+  // console.log(updateUser.modifiedCount);
+
+
+
+}
+
